0236c0de0a35db5f3e917efe586573cb68c59e0b
Seth <setha@hey.com>
2023-01-06 08:57:51 -0600

    move copyright to main page footer

--- src/handler.ts
+++ src/handler.ts
@@ -13,10 +13,6 @@ const responseHeaders = {
 export async function handleRequest(event: FetchEvent): Promise<Response> {
   const { pathname, searchParams } = new URL(event.request.url);
 
-  if (pathname === '/copyright') {
-    return new Response(copyright(), { ...responseHeaders })
-  }
-
   const centralTimeDate = new Date().toLocaleString('en-US', {
     timeZone: 'America/Chicago',
   })
@@ -41,36 +37,6 @@ export async function handleRequest(event: FetchEvent): Promise<Response> {
   return new Response('Date not found in list', { status: 404 })
 }
 
-const copyright = () => {
-  return `
-    <!DOCTYPE html>
-    <html lang='en'>
-    <head>
-      <meta charset='UTF-8'>
-      <meta name='viewport'
-      content='width=device-width, initial-scale=1.0, maximum-scale=5.0, minimum-scale=1.0'>
-      <meta http-equiv='X-UA-Compatible' content='ie=edge'>
-      <title>Copyright</title>
-      <style>
-        body {
-          font-family: ui-sans-serif, system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji";
-          margin: 30px auto;
-          max-width: 750px;
-          padding: 0 30px;
-        }
-      </style>
-    </head>
-    <body>
-      <h1 style='text-align:center;'>Copyright</h1>
-      <div style='text-align:center'>
-        <p>Scripture quotations are from the ESV® Bible (The Holy Bible, English Standard Version®), copyright © 2001 by Crossway, a publishing ministry of Good News Publishers. Used by permission. All rights reserved. The ESV text may not be quoted in any publication made available to the public by a Creative Commons license. The ESV may not be translated into any other language.</p>
-        <p>Users may not copy or download more than 500 verses of the ESV Bible or more than one half of any book of the ESV Bible.</p>
-      </div>
-      </body>
-    </html>
-  `
-}
-
 const getPassages = async (passages: ReadingList, event: FetchEvent) => {
   const passage1 = passages.OT.replace(' ', '+')
   const passage2 = passages.NT.replace(' ', '+')
@@ -85,11 +51,9 @@ const getPassages = async (passages: ReadingList, event: FetchEvent) => {
 
 const getPassageHTML = async (passage: string, event: FetchEvent) => {
   const request = event.request
-  const URI = `https://api.esv.org/v3/passage/html/?q=${passage}&include-audio-link=false`
-  const cacheUrl = new URL(URI)
-  const cacheKey = new Request(cacheUrl.toString(), request)
+  const URI = `https://api.esv.org/v3/passage/html/?q=${passage}&include-audio-link=false&include-short-copyright=false`
   const cache = caches.default
-  let response = await cache.match(cacheKey)
+  let response = await cache.match(URI)
 
   if (!response) {
     response = await fetch(URI, {
@@ -102,7 +66,7 @@ const getPassageHTML = async (passage: string, event: FetchEvent) => {
 
     response.headers.append('Cache-Control', 's-max-age=3600')
 
-    event.waitUntil(cache.put(cacheKey, response.clone()))
+    event.waitUntil(cache.put(URI, response.clone()))
   }
   return response
 }
@@ -160,7 +124,9 @@ const generateHTML = ({
         ${firstPassage}
         ${secondPassage}
         <div style='text-align:center'>
-          <a href='/copyright'>Copyright</a>
+          <p>Copyright</p>
+          <p>Scripture quotations are from the ESV® Bible (The Holy Bible, English Standard Version®), copyright © 2001 by Crossway, a publishing ministry of Good News Publishers. Used by permission. All rights reserved. The ESV text may not be quoted in any publication made available to the public by a Creative Commons license. The ESV may not be translated into any other language.</p>
+          <p>Users may not copy or download more than 500 verses of the ESV Bible or more than one half of any book of the ESV Bible.</p>
         </div>
       </body>
     </html>
